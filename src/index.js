2433689dc65219cedeaefa5b68fa9adf400167fd
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-05-28 09:10:44 +0530

    added sequelize cli

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const apiRoutes = require("./routes/index");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
