abc91e32172103086571a0a9356173082a7fcc4a
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-03 05:49:43 +0530

    added error handling in middleware layer

--- src/middlewares/airplane-middleware.js
+++ src/middlewares/airplane-middleware.js
@@ -1,13 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
 
 const { ErrorResponse } = require("../utils/common");
+const AppError = require("../utils/errors/app-error");
 
 function validateCreateRequest(req, res, next) {
   if (!req.body.modelNumber) {
     ErrorResponse.message = "Something went wrong while creating an airplane";
-    ErrorResponse.error = {
-      explanation: "Model Number not found in the incoming request",
-    };
+    ErrorResponse.error = new AppError(
+      ["Model Number not found in the incoming request"],
+      StatusCodes.BAD_REQUEST
+    );
     return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
   }
   next();
