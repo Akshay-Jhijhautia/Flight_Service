c9ed0a0e7236821cd47ac145271e6eed18cc2e9b
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-18 17:12:14 +0530

    added create flights api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,10 +5,12 @@ const { InfoController } = require("../../controllers");
 const airplaneRoutes = require("./airplane-routes");
 const cityRoutes = require("./city-routes");
 const airportroutes = require("./airport-routes");
+const flightRoutes = require("./flight-routes");
 
 router.use("/airplanes", airplaneRoutes);
 router.use("/cities", cityRoutes);
 router.use("/airports", airportroutes);
+router.use("/flights", flightRoutes);
 
 router.get("/info", InfoController.info);
 
