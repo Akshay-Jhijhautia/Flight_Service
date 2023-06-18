4e30bd8c176eb2a29e8d9e2ca8c6187a116539c9
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2023-06-18 15:53:36 +0530

    added crud for airport

--- src/middlewares/city-middleware.js
+++ src/middlewares/city-middleware.js
@@ -4,7 +4,7 @@ const { ErrorResponse } = require("../utils/common");
 const AppError = require("../utils/errors/app-error");
 
 function validateCreateRequest(req, res, next) {
-  if (!req.body.modelNumber) {
+  if (!req.body.name) {
     ErrorResponse.message = "Something went wrong while creating a city";
     ErrorResponse.error = new AppError(
       ["City name not found in the incoming request"],
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const { InfoController } = require("../../controllers");
 const airplaneRoutes = require("./airplane-routes");
 const cityRoutes = require("./city-routes");
+const airportroutes = require("./airport-routes");
 
 router.use("/airplanes", airplaneRoutes);
 router.use("/cities", cityRoutes);
+router.use("/airports", airportroutes);
 
 router.get("/info", InfoController.info);
 
